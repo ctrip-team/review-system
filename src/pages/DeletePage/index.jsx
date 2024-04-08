bbfdf579c0cc2253e04d0b58d2e7c4a719d46005
yz <274804397@qq.com>
2024-04-08 13:08:19 +0800

    新增水印，保护用户隐私

--- src/pages/DeletePage/index.jsx
+++ src/pages/DeletePage/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Table, Space, Tag, Image, Modal, Input, message } from 'antd';
 import { getDeleteTravelsAPI, recoverTravelAPI } from '../../apis/travel';
+import { useSelector } from 'react-redux';
+import { WaterMark } from '@ant-design/pro-components'
 import {
     Player,
     ControlBar,
@@ -27,7 +29,7 @@ function DeletePage() {
     const [recoverId, setRecoverId] = useState('')
     const [operation, setOperation] = useState('')
     const [content, setContent] = useState('')
-
+    const { roleInfo } = useSelector(state => state.role)
     useEffect(() => {
         async function getTravels() {
             const res = await getDeleteTravelsAPI()
@@ -61,122 +63,124 @@ function DeletePage() {
     }
     return (
         <>
-            <Table
-                bordered={true}
-                dataSource={deleteTravelList}
-                pagination={{ defaultPageSize: 5, showSizeChanger: true, onShowSizeChange, pageSizeOptions: ['5', '10', '20'] }}
-                scroll={{
-                    y: 600,
-                }}
-            >
-                <Column
-                    title="图片"
-                    dataIndex="imgs"
-                    key="imgs"
-                    align="center"
-                    render={
-                        (_, record) => {
-                            if (record.video_url) {
-                                return (
-                                    <Player
-                                        playsInline
-                                        src={record.video_url}
-                                        className='player'
-                                    >
-                                        <BigPlayButton position="center" />
-                                        <ControlBar autoHide={false}>
-                                            <ForwardControl seconds={5} order={3.1} />
-                                            <ReplayControl seconds={5} order={1.1} />
-                                            <PlayToggle />
-                                            <CurrentTimeDisplay order={4.1} />
-                                            <TimeDivider order={4.2} />
-                                            <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5]} order={7.1} />
-                                            <VolumeMenuButton />
-                                        </ControlBar>
-                                    </Player>
-                                )
-                            } else {
-                                return (
-                                    <Image.PreviewGroup
-                                        items={record.imgs}>
-                                        <Image width={200} src={record.imgs[0]} />
-                                    </Image.PreviewGroup>
-                                )
+            <WaterMark content={roleInfo.username}>
+                <Table
+                    bordered={true}
+                    dataSource={deleteTravelList}
+                    pagination={{ defaultPageSize: 5, showSizeChanger: true, onShowSizeChange, pageSizeOptions: ['5', '10', '20'] }}
+                    scroll={{
+                        y: 600,
+                    }}
+                >
+                    <Column
+                        title="图片"
+                        dataIndex="imgs"
+                        key="imgs"
+                        align="center"
+                        render={
+                            (_, record) => {
+                                if (record.video_url) {
+                                    return (
+                                        <Player
+                                            playsInline
+                                            src={record.video_url}
+                                            className='player'
+                                        >
+                                            <BigPlayButton position="center" />
+                                            <ControlBar autoHide={false}>
+                                                <ForwardControl seconds={5} order={3.1} />
+                                                <ReplayControl seconds={5} order={1.1} />
+                                                <PlayToggle />
+                                                <CurrentTimeDisplay order={4.1} />
+                                                <TimeDivider order={4.2} />
+                                                <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5]} order={7.1} />
+                                                <VolumeMenuButton />
+                                            </ControlBar>
+                                        </Player>
+                                    )
+                                } else {
+                                    return (
+                                        <Image.PreviewGroup
+                                            items={record.imgs}>
+                                            <Image width={200} src={record.imgs[0]} />
+                                        </Image.PreviewGroup>
+                                    )
+                                }
                             }
                         }
-                    }
-                />
-                <Column
-                    title="标题"
-                    dataIndex="title"
-                    key="title"
-                    align="center"
-                    render={(_, record) => {
-                        return (
-                            <TextArea
-                                showCount
-                                maxLength={30}
-                                defaultValue={record.title}
-                                disabled
-                                style={{
-                                    height: 80,
-                                    resize: 'none',
-                                    border: 'none'
-                                }}
-                            />
-                        )
-                    }}
-                />
-                <Column
-                    title="文案"
-                    dataIndex="content"
-                    key="content"
-                    align="center"
+                    />
+                    <Column
+                        title="标题"
+                        dataIndex="title"
+                        key="title"
+                        align="center"
+                        render={(_, record) => {
+                            return (
+                                <TextArea
+                                    showCount
+                                    maxLength={30}
+                                    defaultValue={record.title}
+                                    disabled
+                                    style={{
+                                        height: 80,
+                                        resize: 'none',
+                                        border: 'none'
+                                    }}
+                                />
+                            )
+                        }}
+                    />
+                    <Column
+                        title="文案"
+                        dataIndex="content"
+                        key="content"
+                        align="center"
 
-                    render={(_, record) => {
-                        return (
-                            <div className="richTextContainer" onClick={() => showAllContent(record.content)}>
-                                <div dangerouslySetInnerHTML={{ __html: record.content }} />
-                            </div>
-                        )
-                    }}
-                />
-                <Column
-                    title="状态"
-                    dataIndex="status"
-                    key="status"
-                    align="center"
+                        render={(_, record) => {
+                            return (
+                                <div className="richTextContainer" onClick={() => showAllContent(record.content)}>
+                                    <div dangerouslySetInnerHTML={{ __html: record.content }} />
+                                </div>
+                            )
+                        }}
+                    />
+                    <Column
+                        title="状态"
+                        dataIndex="status"
+                        key="status"
+                        align="center"
 
-                    render={() => {
-                        return (
-                            <Tag color='gray'>
-                                已删除
-                            </Tag>
-                        );
+                        render={() => {
+                            return (
+                                <Tag color='gray'>
+                                    已删除
+                                </Tag>
+                            );
 
-                    }}
-                />
-                <Column
-                    title="操作"
-                    key="action"
-                    align="center"
-                    render={(_, record) => (
-                        <Space size="middle">
-                            <a onClick={() => handleRecover(record)}>恢复数据</a>
-                        </Space>
-                    )}
-                />
-            </Table>
+                        }}
+                    />
+                    <Column
+                        title="操作"
+                        key="action"
+                        align="center"
+                        render={(_, record) => (
+                            <Space size="middle">
+                                <a onClick={() => handleRecover(record)}>恢复数据</a>
+                            </Space>
+                        )}
+                    />
+                </Table>
 
-            <Modal title="详细文案" footer={null} closable={false} open={contentModalOpen} onCancel={contentModalCancel} style={{ overflowY: 'auto', height: '80%' }} className='contentModal'>
-                <div>
-                    <div dangerouslySetInnerHTML={{ __html: content }} />
-                </div>
-            </Modal>
+                <Modal title="详细文案" footer={null} closable={false} open={contentModalOpen} onCancel={contentModalCancel} style={{ overflowY: 'auto', height: '80%' }} className='contentModal'>
+                    <div>
+                        <div dangerouslySetInnerHTML={{ __html: content }} />
+                    </div>
+                </Modal>
 
-            <Modal title="恢复数据" open={recoverModalOpen} onOk={confirmRecover} onCancel={() => setRecoverModalOpen(false)}>
-                <p>确定要恢复这个数据吗？</p>
-            </Modal>
+                <Modal title="恢复数据" open={recoverModalOpen} onOk={confirmRecover} onCancel={() => setRecoverModalOpen(false)}>
+                    <p>确定要恢复这个数据吗？</p>
+                </Modal>
+            </WaterMark>
         </>
     )
 }
--- src/pages/ReviewContent/index.jsx
+++ src/pages/ReviewContent/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Table, Space, Tag, Image, Modal, Form, Select, Input, Button, message } from 'antd';
 import { deleteTravelAPI, getTravelsAPI, passTravelAPI, rejectTravelAPI } from '../../apis/travel';
 import { useSelector } from 'react-redux';
+import { WaterMark } from '@ant-design/pro-components'
 import {
     Player,
     ControlBar,
@@ -87,7 +88,6 @@ function ReviewContent() {
         setOperationType('delete')
     }
 
-
     // 展示详细文案
     const showAllContent = (content) => {
         setContent(content)
@@ -101,187 +101,189 @@ function ReviewContent() {
 
     return (
         <>
-            <Table
-                bordered={true}
-                dataSource={travelList}
-                pagination={{ defaultPageSize: 5, showSizeChanger: true, onShowSizeChange, pageSizeOptions: ['5', '10', '20'] }}
-                scroll={{
-                    y: 600,
-                }}
-            >
-                {/* 渲染图片集 */}
-                <Column
-                    title="图片"
-                    dataIndex="imgs"
-                    key="imgs"
-                    align="center"
-                    render={
-                        (_, record) => {
-                            if (record.video_url) {
-                                return (
-                                    <Player
-                                        playsInline
-                                        src={record.video_url}
-                                        className='player'
-                                    >
-                                        <BigPlayButton position="center" />
-                                        <ControlBar autoHide={false}>
-                                            <ForwardControl seconds={5} order={3.1} />
-                                            <ReplayControl seconds={5} order={1.1} />
-                                            <PlayToggle />
-                                            <CurrentTimeDisplay order={4.1} />
-                                            <TimeDivider order={4.2} />
-                                            <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5]} order={7.1} />
-                                            <VolumeMenuButton />
-                                        </ControlBar>
-                                    </Player>
-                                )
-                            } else {
-                                return (
-                                    <Image.PreviewGroup
-                                        items={record.imgs}>
-                                        <Image width={200} src={record.imgs[0]} />
-                                    </Image.PreviewGroup>
-                                )
-                            }
-                        }
-                    }
-                />
-                <Column
-                    title="标题"
-                    dataIndex="title"
-                    key="title"
-                    align="center"
-                    width={'18%'}
-                    render={(_, record) => {
-                        return (
-                            <TextArea
-                                showCount
-                                maxLength={30}
-                                defaultValue={record.title}
-                                disabled
-                                style={{
-                                    height: 80,
-                                    resize: 'none',
-                                    border: 'none'
-                                }}
-                            />
-                        )
-                    }}
-                />
-                <Column
-                    title="文案"
-                    dataIndex="content"
-                    key="content"
-                    align="center"
-                    render={(_, record) => {
-                        return (
-                            <div className="richTextContainer" onClick={() => showAllContent(record.content)}>
-                                <div dangerouslySetInnerHTML={{ __html: record.content }} />
-                            </div>
-                        )
-                    }}
-                />
-                <Column
-                    title="状态"
-                    dataIndex="status"
-                    key="status"
-                    align="center"
-                    width={'10%'}
-                    render={(status) => {
-                        let color = 'green'
-                        let tag = '已通过'
-                        if (status === '1') {
-                            color = 'volcano';
-                            tag = '未通过'
-                        }
-                        else if (status === '0') {
-                            color = 'geekblue'
-                            tag = '待审核'
-                        }
-                        return (
-                            <Tag color={color}>
-                                {tag}
-                            </Tag>
-                        );
-
-                    }}
-                    filters={[
-                        {
-                            text: '待审核',
-                            value: '0'
-                        },
-                        {
-                            text: '未通过',
-                            value: '1'
-                        },
-
-                        {
-                            text: '通过',
-                            value: '2'
-                        }
-                    ]}
-
-                    onFilter={(value, record) => record.status === value}
-                />
-                <Column
-                    title="操作"
-                    key="action"
-                    align="center"
-                    width={'16%'}
-                    render={(_, record) => (
-                        <Space size="middle">
-                            <a onClick={() => handlePass(record)}>通过</a>
-                            <a onClick={() => handleReject(record)}>拒绝</a>
-                            {is_admin ? <a onClick={() => handleDelete(record)}>删除</a> : null}
-                        </Space>
-                    )}
-                />
-            </Table>
-
-            <Modal title="拒绝通过" footer={null} open={isRejectModalOpen} onCancel={() => setIsRejectModalOpen(false)}>
-                <Form
-                    form={form}
-                    onFinish={confirmReject}
-                    labelCol={{
-                        span: 8,
-                    }}
-                    wrapperCol={{
-                        span: 8,
+            <WaterMark content={roleInfo.username}>
+                <Table
+                    bordered={true}
+                    dataSource={travelList}
+                    pagination={{ defaultPageSize: 5, showSizeChanger: true, onShowSizeChange, pageSizeOptions: ['5', '10', '20'] }}
+                    scroll={{
+                        y: 600,
                     }}
                 >
-                    <Form.Item label="快速填写" name="quickFill">
-                        <Select onChange={onChange}>
-                            <Select.Option value="违规图片">违规图片</Select.Option>
-                            <Select.Option value="违规内容">违规内容</Select.Option>
-                        </Select>
-                    </Form.Item>
+                    {/* 渲染图片集 */}
+                    <Column
+                        title="图片"
+                        dataIndex="imgs"
+                        key="imgs"
+                        align="center"
+                        render={
+                            (_, record) => {
+                                if (record.video_url) {
+                                    return (
+                                        <Player
+                                            playsInline
+                                            src={record.video_url}
+                                            className='player'
+                                        >
+                                            <BigPlayButton position="center" />
+                                            <ControlBar autoHide={false}>
+                                                <ForwardControl seconds={5} order={3.1} />
+                                                <ReplayControl seconds={5} order={1.1} />
+                                                <PlayToggle />
+                                                <CurrentTimeDisplay order={4.1} />
+                                                <TimeDivider order={4.2} />
+                                                <PlaybackRateMenuButton rates={[2, 1.5, 1, 0.5]} order={7.1} />
+                                                <VolumeMenuButton />
+                                            </ControlBar>
+                                        </Player>
+                                    )
+                                } else {
+                                    return (
+                                        <Image.PreviewGroup
+                                            items={record.imgs}>
+                                            <Image width={200} src={record.imgs[0]} />
+                                        </Image.PreviewGroup>
+                                    )
+                                }
+                            }
+                        }
+                    />
+                    <Column
+                        title="标题"
+                        dataIndex="title"
+                        key="title"
+                        align="center"
+                        width={'18%'}
+                        render={(_, record) => {
+                            return (
+                                <TextArea
+                                    showCount
+                                    maxLength={30}
+                                    defaultValue={record.title}
+                                    disabled
+                                    style={{
+                                        height: 80,
+                                        resize: 'none',
+                                        border: 'none'
+                                    }}
+                                />
+                            )
+                        }}
+                    />
+                    <Column
+                        title="文案"
+                        dataIndex="content"
+                        key="content"
+                        align="center"
+                        render={(_, record) => {
+                            return (
+                                <div className="richTextContainer" onClick={() => showAllContent(record.content)}>
+                                    <div dangerouslySetInnerHTML={{ __html: record.content }} />
+                                </div>
+                            )
+                        }}
+                    />
+                    <Column
+                        title="状态"
+                        dataIndex="status"
+                        key="status"
+                        align="center"
+                        width={'10%'}
+                        render={(status) => {
+                            let color = 'green'
+                            let tag = '已通过'
+                            if (status === '1') {
+                                color = 'volcano';
+                                tag = '未通过'
+                            }
+                            else if (status === '0') {
+                                color = 'geekblue'
+                                tag = '待审核'
+                            }
+                            return (
+                                <Tag color={color}>
+                                    {tag}
+                                </Tag>
+                            );
 
-                    <Form.Item
-                        label="拒绝原因"
-                        name="reason"
-                        rules={[{ required: true, message: '请输入拒绝原因！' }]}
+                        }}
+                        filters={[
+                            {
+                                text: '待审核',
+                                value: '0'
+                            },
+                            {
+                                text: '未通过',
+                                value: '1'
+                            },
+
+                            {
+                                text: '通过',
+                                value: '2'
+                            }
+                        ]}
+
+                        onFilter={(value, record) => record.status === value}
+                    />
+                    <Column
+                        title="操作"
+                        key="action"
+                        align="center"
+                        width={'16%'}
+                        render={(_, record) => (
+                            <Space size="middle">
+                                <a onClick={() => handlePass(record)}>通过</a>
+                                <a onClick={() => handleReject(record)}>拒绝</a>
+                                {is_admin ? <a onClick={() => handleDelete(record)}>删除</a> : null}
+                            </Space>
+                        )}
+                    />
+                </Table>
+
+                <Modal title="拒绝通过" footer={null} open={isRejectModalOpen} onCancel={() => setIsRejectModalOpen(false)}>
+                    <Form
+                        form={form}
+                        onFinish={confirmReject}
+                        labelCol={{
+                            span: 8,
+                        }}
+                        wrapperCol={{
+                            span: 8,
+                        }}
                     >
-                        <Input.TextArea showCount maxLength={100} style={{ resize: 'none', width: 200, height: 100 }} />
-                    </Form.Item>
+                        <Form.Item label="快速填写" name="quickFill">
+                            <Select onChange={onChange}>
+                                <Select.Option value="违规图片">违规图片</Select.Option>
+                                <Select.Option value="违规内容">违规内容</Select.Option>
+                            </Select>
+                        </Form.Item>
 
-                    <Form.Item wrapperCol={{ offset: 10 }}>
-                        <Button type="primary" htmlType="submit">
-                            确认
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Modal >
+                        <Form.Item
+                            label="拒绝原因"
+                            name="reason"
+                            rules={[{ required: true, message: '请输入拒绝原因！' }]}
+                        >
+                            <Input.TextArea showCount maxLength={100} style={{ resize: 'none', width: 200, height: 100 }} />
+                        </Form.Item>
 
-            <Modal title="删除" open={isDeleteModalOpen} onOk={confirmDelete} onCancel={() => setIsDeleteModalOpen(false)}>
-                <p>确定要删除吗？</p>
-            </Modal>
+                        <Form.Item wrapperCol={{ offset: 10 }}>
+                            <Button type="primary" htmlType="submit">
+                                确认
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Modal >
 
-            <Modal title="详细文案" footer={null} closable={false} open={contentModalOpen} onCancel={contentModalCancel} style={{ overflowY: 'auto', height: '80%' }} className='contentModal'>
-                <div>
-                    <div dangerouslySetInnerHTML={{ __html: content }} />
-                </div>
-            </Modal>
+                <Modal title="删除" open={isDeleteModalOpen} onOk={confirmDelete} onCancel={() => setIsDeleteModalOpen(false)}>
+                    <p>确定要删除吗？</p>
+                </Modal>
+
+                <Modal title="详细文案" footer={null} closable={false} open={contentModalOpen} onCancel={contentModalCancel} style={{ overflowY: 'auto', height: '80%' }} className='contentModal'>
+                    <div>
+                        <div dangerouslySetInnerHTML={{ __html: content }} />
+                    </div>
+                </Modal>
+            </WaterMark>
         </>
     )
 }
